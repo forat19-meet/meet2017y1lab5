1cf2c632381db0afc0d9aafcf430897e5cda0529
forat19-meet <forat19@meet.mit.edu>
2017-07-27 12:11:25 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,17 @@
+def add_numbers():
+    answer = answer + 1
+    
+answer = add_numbers()
+print(answer)
+
+
+
+
+
+
+c = 0
+for number in range (1, 100 + 1):
+    print(number)
+    c = c + number
+print(c)
+
