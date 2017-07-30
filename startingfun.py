bd9f0c93e2e1d6e89d35af45ba4e58760e05159d
forat19-meet <forat19@meet.mit.edu>
2017-07-30 18:30:01 +0300

    Stopping here

---
+++ startingfun.py
@@ -0,0 +1,7 @@
+def starting_test(s):
+    x = len(s)
+    if x>2 and s[0] == s[x-1]:
+        print("true")
+    else:
+        print("false")
+        
---
+++ tables.py
@@ -0,0 +1,12 @@
+
+dog = 'dalmation'
+dogs = [dog,"poodle","boxer"]
+len(dogs)
+
+
+
+
+
+a = ["she","seels","sea","shells","by","the","sea","shore"]
+b = "selfish shellfish"
+c = [1, 1, 2, 3, 5, 8, 13]
