9d9fc7ad347fc421aff9cf76dcb9eea419bc3633
forat19-meet <forat19@meet.mit.edu>
2017-07-30 12:54:46 +0200

    Stopping here

---
+++ eddwe.py
@@ -0,0 +1,13 @@
+def add_numbers(start, end):
+    c=0
+    for i in range (start, end+1):
+        #print (i)
+        c=c+i
+    return (c)
+
+answer = add_numbers(1,2)
+print(answer)
+#test2 = add_numbers(1, 100)
+#print(test2)
+#test3 = add_numbers(1000, 5000)
+#print(test3)
